a35fc16247924b1f5bed132ad670763bbd079a4a
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-07-01 21:03:20 +0300

    Assignment complete

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int yıl;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yılı Giriniz : ");
+        yıl = scanner.nextInt();
+
+        if (yıl % 400 == 0 || (yıl % 4 == 0 && yıl % 100 != 0)){
+            System.out.println(yıl + " bir atık yıldır !" );
+        } else {
+            System.out.println(yıl + " bir atık yıl değildir !" );
+        }
+    }
 }
